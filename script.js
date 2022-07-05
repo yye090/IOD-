897eee0b863b9a3a9284781435d25dffe11aa1b9
unknown <yye090@aucklanduni.ac.nz>
2022-07-05 15:16:58 +1200

    new commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function helloworld(){
+    alert("Hello World");
+    
+    document.getElementById('demo').innerHTML= "Hello Javascript";
+    document.getElementById('demo').style.backgroundColor="black";
+}
+
